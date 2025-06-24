f658e5ce57c29780aee357b290527864bfda5197
Shivam kumar <artkum9@gmail.com>
2025-06-24 16:46:30 +0530

    final touch backend

--- myapp/urls.py
+++ myapp/urls.py
@@ -13,4 +13,27 @@ urlpatterns =[
     path('tata/',tata, name='tata'),
     path('tork/',tork, name='tork'),
     path('tvs/',tvs, name='tvs'),
+
+    path('mahindra1/',mahindra1,name='mahindra1'),
+    path('mahindra2/',mahindra2,name='mahindra2'),
+    path('mahindra3/',mahindra3,name='mahindra3'),
+    path('mahindra4/',mahindra4,name='mahindra4'),
+    
+    path('tata1/',tata1,name='tata1'),
+    path('tata2/',tata2,name='tata2'),
+    path('tata3/',tata3,name='tata3'),
+    path('tata4/',tata4,name='tata4'),
+
+    path('royal1/',royal1,name='royal1'),
+    path('royal2/',royal2,name='royal2'),
+
+    path('tvs1/',tvs1,name='tvs1'),
+    path('tvs2/',tvs2,name='tvs2'),
+
+    path('revolt1/',revolt1,name='revolt1'),
+    path('revolt2/',revolt2,name='revolt2'),
+
+    path('ola1/',ola1,name='ola1'),
+
+    path('tork1/',tork1,name='tork1')
             ]
--- myapp/views.py
+++ myapp/views.py
@@ -59,3 +59,67 @@ def tork(request):
 def tvs(request):
 
     return render(request,'tvs.html')
+
+def mahindra1(request):
+
+    return redirect('/mahindra/#carousel1')
+
+def mahindra2(request):
+
+    return redirect('/mahindra/#carousel2')
+
+def mahindra3(request):
+
+    return redirect('/mahindra/#carousel3')
+
+def mahindra4(request):
+
+    return redirect('/mahindra/#carousel4')
+
+def tata1(request):
+
+    return redirect('/tata/#carousel1')
+
+def tata2(request):
+
+    return redirect('/tata/#carousel2')
+
+def tata3(request):
+
+    return redirect('/tata/#carousel3')
+
+def tata4(request):
+
+    return redirect('/tata/#carousel4')
+
+def royal1(request):
+
+    return redirect('/royal/#carousel1')
+
+def royal2(request):
+
+    return redirect('/royal/#carousel2')
+
+def tvs1(request):
+
+    return redirect('/tvs/#carousel1')
+
+def tvs2(request):
+
+    return redirect('/tvs/#carousel2')
+
+def revolt1(request):
+
+    return redirect('/revolt/#carousel1')
+
+def revolt2(request):
+
+    return redirect('/revolt/#carousel2')
+
+def ola1(request):
+
+    return redirect('/ola/#carousel1')
+
+def tork1(request):
+
+    return redirect('/tork/#carousel1')
